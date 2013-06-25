a6c2f21ebfb7c16a9fe83a67c727c0a43079b53e
0xD34D <clark@scheffsblend.com>
2013-06-24 19:55:11 -0700

    Add gesture based lockscreen security. [1/2] This adds the ability to us a hand drawn gesture as a means to unlock the device.
    In the case that the gesture is forgotton or not
    being recognized correctly, the fallback will be the
    same as when using patterns.
    
    PS2:
    Return SecurityMode.Account as backup for gesture login.
    
    PS3:
    Poke the wakelock when a gesture is detected so the screen
    will not timeout when an invalid gesture is entered.
    
    PS4:
    Clean things up a bit.
    
    Change-Id: I4ca8d7edff0a8906220cf4561d8b90896520e0cd
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/java/com/android/internal/widget/LockPatternUtils.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4570,6 +4570,8 @@ public final class Settings {
             MOVED_TO_LOCK_SETTINGS.add(Secure.LOCK_DOTS_VISIBLE);
             MOVED_TO_LOCK_SETTINGS.add(Secure.LOCK_PATTERN_TACTILE_FEEDBACK_ENABLED);
             MOVED_TO_LOCK_SETTINGS.add(Secure.LOCK_SYNC_ENCRYPTION_PASSWORD);
+            MOVED_TO_LOCK_SETTINGS.add(Secure.LOCK_GESTURE_ENABLED);
+            MOVED_TO_LOCK_SETTINGS.add(Secure.LOCK_GESTURE_VISIBLE);
 
             MOVED_TO_GLOBAL = new HashSet<String>();
             MOVED_TO_GLOBAL.add(Settings.Global.ADB_ENABLED);
@@ -5211,6 +5213,18 @@ public final class Settings {
          * Whether lockscreen error pattern is visible (0 = false, 1 = true)
          */
         public static final String LOCK_SHOW_ERROR_PATH = "lock_pattern_show_error_path";
+
+        /**
+         * Whether autolock is enabled (0 = false, 1 = true)
+         * @hide
+         */
+        public static final String LOCK_GESTURE_ENABLED = "lock_gesture_autolock";
+
+        /**
+         * Whether lock gesture is visible as user enters (0 = false, 1 = true)
+         * @hide
+         */
+        public static final String LOCK_GESTURE_VISIBLE = "lock_gesture_visible_pattern";
 
         /**
          * This preference allows the device to be locked given time after screen goes off,
