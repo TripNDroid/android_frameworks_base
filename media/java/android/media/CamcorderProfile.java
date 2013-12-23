675ce5a71fa80ace2e4f1bf3c6e0dda039f3f8df
TripNRaVeR <noerialexeew@live.nl>
2013-12-23 07:04:54 +0100

    add slowmotion media profile support

--- media/java/android/media/CamcorderProfile.java
+++ media/java/android/media/CamcorderProfile.java
@@ -120,9 +120,14 @@ public class CamcorderProfile
 
     public static final int QUALITY_4kDCI = 13;
 
+    /**
+    * {@hide}
+    */
+    public static final int QUALITY_SLOWMOTION = 14;
+
     // Start and end of quality list
     private static final int QUALITY_LIST_START = QUALITY_LOW;
-    private static final int QUALITY_LIST_END = QUALITY_4kDCI;
+    private static final int QUALITY_LIST_END = QUALITY_SLOWMOTION;
 
     /**
      * Time lapse quality level corresponding to the lowest available resolution.
