e24423dfac3477bb453abbc73c3204de65b86a0b
Alex Cruz <mazdarider23@gmail.com>
2016-02-06 03:59:14 -0500

    Add option to hide lockscreen clock, date, & alarm text [1/2]
    Credit to @beanstown106 for his alarm text mod
    
    Change-Id: I47c98045b9b7e1833c5904d1be9f5bd412a7eeae

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3890,6 +3890,24 @@ public final class Settings {
          */
         public static final String CUSTOM_CARRIER_LABEL = "custom_carrier_label";
 
+         /**
+         * Hide lockscreen date
+         * @hide
+         */
+        public static final String HIDE_LOCKSCREEN_DATE = "hide_lockscreen_date";
+
+        /**
+         * Hide lockscreen clock
+         * @hide
+         */
+        public static final String HIDE_LOCKSCREEN_CLOCK = "hide_lockscreen_clock";
+
+        /**
+         * Hide lockscreen alarm
+         * @hide
+         */
+        public static final String HIDE_LOCKSCREEN_ALARM = "hide_lockscreen_alarm";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
