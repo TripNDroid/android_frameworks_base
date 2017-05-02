db1710a48aeab4c3fd6a6c26afc429333722ba5b
TripNRaVeR <noerialexeew@live.nl>
2017-05-03 01:14:46 +0200

    Power Menu animations [1/2]
    Change-Id: If0ab7e79f01e55c86a701fd91f1d36e02ff206d1

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3753,6 +3753,13 @@ public final class Settings {
         public static final String MEDIA_SCANNER_ON_BOOT = "media_scanner_on_boot";
 
         /**
+         * Whether to display set of animations when prompting the power menu
+         *
+         * @hide
+         */
+        public static final String POWER_MENU_ANIMATIONS = "power_menu_animations";
+
+        /**
          * Show or hide clock
          * 0 - hide
          * 1 - show (default)
